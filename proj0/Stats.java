cf492d558bd552ea3c75c2e29a7ec004c6e386f8
clara <clarajylan@gmail.com>
2020-03-24 14:17:18 -0400

    chores: solution for exercise2, stats.java

--- proj0/Stats.java
+++ proj0/Stats.java
@@ -2,19 +2,20 @@ import java.util.concurrent.atomic.DoubleAccumulator;
 
 public class Stats{
   public static void main(String[] args){
+    // the first input(args[0]) declare size of the input array
     int n = Integer.parseInt(args[0]);
-    double[] a = new double[n];
+    double[] numbers = new double[n];
     for ( int i = 0; i < n; i++){
-      a[i] = StdIn.readDouble();
+      numbers[i] = StdIn.readDouble();
     }
     double sum = 0.0;
     for(int i = 0; i < n; i++){
-      sum += a[i];
+      sum += numbers[i];
     }
     double mean = sum / n;
     double sum2 = 0.0;
     for (int i = 0; i < n; i++){
-      sum2 += (a[i] - mean) * (a[i] - mean);
+      sum2 += (numbers[i] - mean) * (numbers[i] - mean);
     }
     double stddev = Math.sqrt(sum2 / (n-1));
     StdOut.println("Mean:" + mean);
