409f4f71bbfa7170adf7f3dc115e11024b4670d3
clara <clarajylan@gmail.com>
2020-04-03 09:58:42 -0400

    feat:finish project1b

--- proj1b/src/OffByN.java
+++ proj1b/src/OffByN.java
@@ -1,8 +1,12 @@
 public class OffByN implements CharacterComparator {
-    private int N;
+    private int diff;
 
+    // make constructor for OffByN to enable params
+    public OffByN(int N){
+        diff = N;
+    }
     @Override
     public boolean equalChars(char x, char y){
-        return true;
+        return x-y == diff || y - x == diff;
     }
 }
